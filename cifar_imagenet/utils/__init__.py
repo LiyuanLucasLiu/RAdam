6fe3acde24fe27c958c371b4ee9dc63a67e6d60c
Akhilesh Gotmare <dg.akhilesh@gmail.com>
2019-09-13 11:58:55 +0800

    UPD final fix for cifar training

--- cifar_imagenet/utils/__init__.py
+++ cifar_imagenet/utils/__init__.py
@@ -8,4 +8,4 @@ from .eval import *
 # progress bar
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
-from progress.bar import Bar as Bar
+from utils.progress.bar import Bar as Bar
